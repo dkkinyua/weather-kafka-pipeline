13fc386a9ec0708c299809eab6bf416e9587efaf
dkkinyua <denzelkinyua11@gmail.com>
2025-06-17 15:22:00 +0300

    Commit: Switched to Confluent Cloud for Kafka Switch from self-managed Apache Kafka to cloud-managed Kafka service by Confluent New env variables initialized

--- weather_producer.py
+++ weather_producer.py
@@ -1,29 +1,46 @@
 import os
 import json
+import time
 import requests
+from confluent_kafka import Producer
 from dotenv import load_dotenv
-from kafka import KafkaProducer
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
-SERVER_ADDRESS = os.getenv("SERVER_ADDRESS")
+API_SECRET_KEY = os.getenv("API_SECRET_KEY")
+BOOTSTRAP_SERVER = os.getenv("BOOTSTRAP_SERVER")
+OPENWEATHER_KEY = os.getenv("OPENWEATHER_KEY")
+TOPIC = os.getenv("CONFLUENT_TOPIC")
+CLIENT_ID = os.getenv("CLIENT_ID")
 
-producer = KafkaProducer(
-    bootstrap_servers = f'{SERVER_ADDRESS}:9092',
-    value_serializer = lambda v: json.dumps(v).encode('utf-8')
-)
+producer_config = {
+    'bootstrap.servers': BOOTSTRAP_SERVER,
+    'security.protocol': 'SASL_SSL',
+    'sasl.mechanism': 'PLAIN',
+    'sasl.username': API_KEY,
+    'sasl.password': API_SECRET_KEY,  
+    'client.id': CLIENT_ID
+}
+
+producer = Producer(producer_config)
+
 cities = ['Nairobi', 'Pretoria', 'Cairo', 'Lagos', 'Mombasa']
 
-def produce_data(cities):
-    for city in cities:
-        url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}'
-        data = requests.get(url).json()
-        return data
+def produce_data(city):
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPENWEATHER_KEY}'
+    data = requests.get(url).json()
+    return {
+        "city": city,
+        "timestamp": int(time.time()),
+        "temperature": data["main"]["temp"],
+        "description": data["weather"][0]["description"]
+    }
     
 if __name__ == '__main__':
-    weather = produce_data(cities)
-    for i in weather:
-        print(f'City weather: {i}')
-        producer.send('weatherdata', i)
-    producer.flush()
-    print("Data sent successfully!")
+    while True:
+        for city in cities:
+            weather = produce_data(city)
+            print(f"Weather: {weather}")
+            producer.produce(TOPIC, json.dumps(weather).encode('utf-8'))
+            producer.flush()
+        print("Data sent successfully!")

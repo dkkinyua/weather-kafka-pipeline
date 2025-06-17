d229661ebe0faa1fc990a573d86ba0df3ca206ab
dkkinyua <denzelkinyua11@gmail.com>
2025-06-17 16:49:47 +0300

    Commit: Consumer complete Consumers now gets data from topic Loads data into Postgres database

--- weather_consumer.py
+++ weather_consumer.py
@@ -1,44 +1,70 @@
 import os
 import json
-import uuid
-from cassandra.cluster import Cluster
-from cassandra.query import SimpleStatement
-from datetime import datetime
+import psycopg2
+from confluent_kafka import Consumer
 from dotenv import load_dotenv
-from kafka import KafkaConsumer
 
 load_dotenv()
-SERVER_ADDRESS = os.getenv("SERVER_ADDRESS")
+API_KEY = os.getenv("API_KEY")
+API_SECRET_KEY = os.getenv("API_SECRET_KEY")
+BOOTSTRAP_SERVER = os.getenv("BOOTSTRAP_SERVER")
+TOPIC = os.getenv("CONFLUENT_TOPIC")
+CLIENT_ID = os.getenv("CLIENT_ID")
+GROUP_ID = os.getenv("GROUP_ID")
 
-consumer = KafkaConsumer(
-    'weatherdata',
-    bootstrap_servers = f'{SERVER_ADDRESS}:9092',
-    value_deserializer = lambda m: json.loads(m.decode('utf-8')),
-    auto_offset_reset='earliest',
-    enable_auto_commit=True
-)
-cluster = Cluster([SERVER_ADDRESS])
-session = cluster.connect('weather')
+consumer_config = {
+    'bootstrap.servers': BOOTSTRAP_SERVER,
+    'security.protocol': 'SASL_SSL',
+    'sasl.mechanism': 'PLAIN',
+    'sasl.username': API_KEY,
+    'sasl.password': API_SECRET_KEY,
+    'group.id': GROUP_ID,
+    'auto.offset.reset': 'earliest',
+    'enable.auto.commit': True
+}
 
-insert_query = session.prepare("""
-    INSERT INTO weatherdata (id, city, temperature, humidity, timestamp)
-    VALUES (?, ?, ?, ?, ?)
-""")
+consumer = Consumer(consumer_config)
+consumer.subscribe([TOPIC])
 
-def consume_data():
-    for message in consumer:
-        data = message.value
+def load_to_db(weather):
+    try:
+        conn = psycopg2.connect(
+            host=os.getenv("DB_HOST"),
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USERNAME"),
+            password=os.getenv("DB_PWD")
+        )
+        cur = conn.cursor()
+        cur.execute(
+            """
+            INSERT INTO african_weather_data (city, description, temperature, timestamp)
+            VALUES (%s, %s, %s, %s)
+            """,
+            (weather['city'], weather['description'], weather['temperature'], weather['timestamp'])
+        )
+        conn.commit()
+        cur.close()
+        conn.close()
+        print(f"Inserted weather data for {weather['city']} into DB.")
+    except Exception as e:
+        print(f"Database error: {e}")
+
+def consume_data(consumer):
+    while True:
+        msg = consumer.poll(1.0) # poll data for 1 second before consuming again
+        if not msg:
+            continue
+        if msg.error():
+            print(f"Consumer error: {msg.error()}")
+            continue
+        weather = json.loads(msg.value().decode('utf-8'))
+        print(weather)
+
         try:
-            session.execute(
-                insert_query,
-                (
-                    uuid.uuid4(),
-                    data['city'],
-                    float(data['temperature']),
-                    float(data['humidity']),
-                    datetime.now()
-                )
-            )
-            print(f"Inserted: {data}")
+            load_to_db(weather)
+            print(f"{weather['city']}, loaded to db at {weather['timestamp']}")
         except Exception as e:
-            print(f"Error inserting: {e}")
+            print(f"Loading to db error: {e}")
+
+if __name__ == '__main__':
+    consume_data(consumer)
